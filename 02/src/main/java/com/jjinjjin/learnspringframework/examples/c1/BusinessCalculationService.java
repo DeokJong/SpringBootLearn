99da27603e225d382ad61027d5696d5006f4e153
DeokJong <novelss444@jbnu.ac.kr>
2024-03-25 20:53:23 +0900

    Add Spring Boot Project

--- 02/src/main/java/com/jjinjjin/learnspringframework/examples/c1/BusinessCalculationService.java
+++ 02/src/main/java/com/jjinjjin/learnspringframework/examples/c1/BusinessCalculationService.java
@@ -1,10 +1,12 @@
 package com.jjinjjin.learnspringframework.examples.c1;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 
-@Component
+//@Component
+@Service //논리가 들어있으면 @Service
 public class BusinessCalculationService{
 
 	private DataService dataService;
--- 02/src/main/java/com/jjinjjin/learnspringframework/examples/c1/MongoDbDataService.java
+++ 02/src/main/java/com/jjinjjin/learnspringframework/examples/c1/MongoDbDataService.java
@@ -2,8 +2,10 @@ package com.jjinjjin.learnspringframework.examples.c1;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+//@Component
+@Repository// 데이터베이스와 상호작용 수행
 @Primary
 public class MongoDbDataService implements DataService{
 	@Override
